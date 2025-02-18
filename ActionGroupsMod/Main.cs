35ff54bc9023c119a9d6e730700846c244847259
AstroTheRabbit <66503946+AstroTheRabbit@users.noreply.github.com>
2025-02-19 08:20:16 +1100

    v1.2: fix auto-updating

--- ActionGroupsMod/Main.cs
+++ ActionGroupsMod/Main.cs
@@ -15,11 +15,11 @@ namespace ActionGroupsMod
         public override string DisplayName => "Action Groups";
         public override string Author => "Astro The Rabbit";
         public override string MinimumGameVersionNecessary => "1.5.10.2";
-        public override string ModVersion => "1.1";
+        public override string ModVersion => "1.2";
         public override string Description => "Adds KSP-like part action groups to SFS.";
 
         public override Dictionary<string, string> Dependencies { get; } = new Dictionary<string, string> { { "UITools", "1.1.5" }, { "customsavedata", "1.0" } };
-        public Dictionary<string, FilePath> UpdatableFiles => new Dictionary<string, FilePath>() { { "https://github.com/AstroTheRabbit/Action-Groups-Mod-SFS/releases/latest/download/ActionGroups.dll", new FolderPath(ModFolder).ExtendToFile("ActionGroups.dll") } };
+        public Dictionary<string, FilePath> UpdatableFiles => new Dictionary<string, FilePath>() { { "https://github.com/AstroTheRabbit/Action-Groups-Mod-SFS/releases/latest/download/ActionGroupsMod.dll", new FolderPath(ModFolder).ExtendToFile("ActionGroupsMod.dll") } };
 
         public override void Early_Load()
         {
